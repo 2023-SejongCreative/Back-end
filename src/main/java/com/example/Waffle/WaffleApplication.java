8e54fb4ee889eefc4d43f5c8e06ceb7d6c7812ac
Ho <ghdy98@naver.com>
2023-03-09 16:33:19 +0900

    chore : 백엔드 프로젝트 생성 및 초기 설정

---
+++ src/main/java/com/example/Waffle/WaffleApplication.java
@@ -0,0 +1,13 @@
+package com.example.Waffle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaffleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Waffle/WaffleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Waffle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaffleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
