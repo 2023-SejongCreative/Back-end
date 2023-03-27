8f87edf56a2820742ccf0bd04be7f3900e4ee6a1
DESKTOP-0PS92PR\전서현 <tjgus5935@gmail.com>
2023-03-27 14:47:13 +0900

    feat : config에 jwt 적용 중

--- src/main/java/com/example/Waffle/config/SecurityConfig.java
+++ src/main/java/com/example/Waffle/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -26,16 +27,27 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        //
         http
                 .cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
-                .headers().frameOptions().disable();
+                .headers().frameOptions().disable()
+                .and()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+
+        ;
         return http.build();
     }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
+        //정적 자원에 스프링 시큐리티 필터 규칙을 적용하지 않도록 설정(서버 부하를 줄이도록)
         return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
---
+++ src/main/java/com/example/Waffle/token/JwtAuthenticationFilter.java
@@ -0,0 +1,5 @@
+package com.example.Waffle.token;
+
+public class JwtAuthenticationFilter {
+
+}
