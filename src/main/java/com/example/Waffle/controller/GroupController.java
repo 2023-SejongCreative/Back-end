b41acf5e3e482b4252fbf5ad45002c905782409a
HoGyeongC <ghdy98@naver.com>
2023-04-09 17:42:01 +0900

    feat : 그룹 목록 불러오기 기능 생성

--- src/main/java/com/example/Waffle/controller/GroupController.java
+++ src/main/java/com/example/Waffle/controller/GroupController.java
@@ -6,8 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -26,5 +25,12 @@ public class GroupController {
 
         return new ResponseEntity<>("그룹 생성에 성공하였습니다.", HttpStatus.OK);
     }
+
+    @GetMapping("/{user_email}/groups")
+    public @ResponseBody String grouplist(@PathVariable("user_email") String email){
+        return groupService.userGrouplist(email);
+    }
+
+
 
 }
